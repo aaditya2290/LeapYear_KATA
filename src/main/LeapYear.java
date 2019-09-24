406f246489efbd4d717da1a9e9e1f1b8f9cb777a
aaditya2290 <aaditya_2290@yahoo.co.in>
2019-09-24 18:53:38 +0530

    test to check if year is divisible by 4 and not divisible by 100 for years between 100 and 399

--- src/main/LeapYear.java
+++ src/main/LeapYear.java
@@ -5,9 +5,14 @@ public class LeapYear {
 	public boolean isLeapYear(int year)
 	{
 		if (year%4==0)
-	    return true;
+		{
+			if (year%100==0)
+				return false;
+			else
+				return true;
+		}
 		else
-		return false;
+			return false;
 	}
 
 }
--- src/test/LeapYearTest.java
+++ src/test/LeapYearTest.java
@@ -9,24 +9,31 @@ import org.junit.Test;
 public class LeapYearTest {
 
 	LeapYear lp;
-	
+
 	@Before
 	public void initializeLeapYear()
 	{
 		lp=new LeapYear();
 	}
-	
+
 	@Test
 	public void testDivisibleBy4_YearsLessThan100()
 	{
 		assertFalse(isLeap(10));
 		assertTrue(isLeap(88));
 	}
-	
+
+	@Test
+	public void testDivisibleBy4NotDivisibleby100_YearsBetween100to399()
+	{
+		assertFalse(isLeap(200));
+		assertTrue(isLeap(248));
+	}
+
 	private boolean isLeap(int year)
 	{
 		return lp.isLeapYear(year);
 	}
-	
+
 
 }
